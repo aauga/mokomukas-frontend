50f7c235a7b41c51b029a526933f0dad3b388812
Adomas Auga <35742185+aauga@users.noreply.github.com>
2024-05-06 22:49:56 +0300

    create useAuthenticatedUser hook

--- src/api/sessions/api.ts
+++ src/api/sessions/api.ts
@@ -1,9 +1,16 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { AxiosResponse } from "axios";
 import { SignupFormProps } from "./types";
 import { User } from "../../types/user";
 import { axiosInstance } from "../../config/axiosInstance";
+
+export function useAuthenticatedUser() {
+  return useQuery<User>({
+    queryFn: async () => (await axiosInstance.get("/sessions")).data,
+    queryKey: ["users"],
+  });
+}
 
 export function useSignup() {
   const queryClient = useQueryClient();
@@ -11,7 +18,9 @@ export function useSignup() {
   return useMutation({
     mutationFn: (data: SignupFormProps) =>
       axiosInstance.post("/users", { user: data }),
-    onSuccess: (res: AxiosResponse<User>) =>
-      queryClient.invalidateQueries({ queryKey: ["users", res.data.id] }),
+    onSuccess: (res: AxiosResponse<User>) => {
+      const user = res.data;
+      queryClient.invalidateQueries({ queryKey: ["users", user.id] });
+    },
   });
 }
