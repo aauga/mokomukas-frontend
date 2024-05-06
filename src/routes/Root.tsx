0adc2442f99731997f8e00cfda829720d00ceee4
Adomas Auga <35742185+aauga@users.noreply.github.com>
2024-05-07 00:25:42 +0300

    fix long retries

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -3,7 +3,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Navbar } from "../components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
 
 export default function Root() {
   return (
