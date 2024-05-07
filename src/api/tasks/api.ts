2d0d17de06ec569d49e57ce0b6085c9f437c9aac
Adomas Auga <35742185+aauga@users.noreply.github.com>
2024-05-07 23:14:40 +0300

    fix query keys

--- src/api/tasks/api.ts
+++ src/api/tasks/api.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 export const useTask = (taskId: number, enabled?: boolean) => {
   return useQuery<Task>({
     queryFn: async () => (await axiosInstance.get(`/tasks/${taskId}`)).data,
-    queryKey: ["tasks"],
+    queryKey: ["tasks", taskId],
     enabled: enabled,
   });
 };
